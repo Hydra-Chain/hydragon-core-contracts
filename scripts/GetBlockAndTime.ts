73feb40ee2cb9030ed00bf31095a11f195967be5
Samuil Borisov <samuil.bo5@gmail.com>
2024-04-23 18:11:32 +0300

    Finish scripts

--- scripts/GetBlockAndTime.ts
+++ scripts/GetBlockAndTime.ts
@@ -1,23 +1,22 @@
-// Run: npx hardhat run scripts/GetTimestamp.ts --network childTest
+// Run: npx hardhat run scripts/GetBlockAndTime.ts --network childTest
 import { ethers } from "hardhat";
 
 async function getCurrentTimestamp() {
-  // Get provider based on network name
   const provider = ethers.provider;
 
-  // Get current block number
   const blockNumber = await provider.getBlockNumber();
-
-  // Get current block
   const block = await provider.getBlock(blockNumber);
-
-  // Get current timestamp from the block
   const currentTimestamp = block.timestamp;
 
-  console.log("Current Timestamp:", currentTimestamp);
+  console.log(`
+  _________________________________________
+
+  Block Number: ${blockNumber}
+  TimeStamp of the block: ${currentTimestamp}
+  _________________________________________
+  `);
 }
 
-// Run the function
 getCurrentTimestamp().catch((error) => {
   console.error(error);
   process.exitCode = 1;
--- scripts/InRangeEvents.ts
+++ scripts/InRangeEvents.ts
@@ -1,20 +1,15 @@
-// Run: npx hardhat run scripts/EventsInRange.ts --network childTest
+// Run: npx hardhat run scripts/InRangeEvents.ts --network childTest
 import { ethers } from "hardhat";
 
-// const day = 60 * 60 * 24;
-// const hour = 60 * 60;
-
 // eslint-disable-next-line no-unused-vars
-const FRI_TIMESTAMP = 1713537000; // 17:30 19.04.2024
-// eslint-disable-next-line no-unused-vars
-const SAT_TIMESTAMP = 1713623400; // 17:30 20.04.2024
+const TIMESTAMP = 1713537000; // 17:30 19.04.2024
 
 // Input parameters for the function:
 const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000105";
 const EVENT_NAME = "ValidatorRewardDistributed";
 const VALIDATOR_ADDRESS = "0x";
-const YOUR_START_TIME_IN_UNIX_TIMESTAMP = FRI_TIMESTAMP - 1000;
-const YOUR_END_TIME_IN_UNIX_TIMESTAMP = FRI_TIMESTAMP;
+const YOUR_START_TIME_IN_UNIX_TIMESTAMP = TIMESTAMP - 1000;
+const YOUR_END_TIME_IN_UNIX_TIMESTAMP = TIMESTAMP;
 
 async function getEventsByIndexedAddress(
   contractAddress: string,
@@ -23,17 +18,14 @@ async function getEventsByIndexedAddress(
   startTime: number,
   endTime: number
 ) {
-  // Get provider based on network name
   const provider = ethers.provider;
   const encodedAddress = ethers.utils.defaultAbiCoder.encode(["address"], [indexedAddress]);
 
-  // Step 1: Create filter to get events by indexed address
   const filter = {
     address: contractAddress,
     topics: [ethers.utils.id(eventName), encodedAddress],
   };
 
-  // Step 2: Get events in the specified time range
   const events = await provider.getLogs(
     Object.assign({}, filter, {
       fromBlock: startTime,
@@ -42,14 +34,17 @@ async function getEventsByIndexedAddress(
   );
 
   console.log(
-    `Events for ${eventName} with indexed address ${indexedAddress} from ${new Date(startTime * 1000)} to ${new Date(
-      endTime * 1000
-    )}:`
+    ` 
+     _________________________________
+
+Events for ${eventName} with indexed address ${indexedAddress} 
+from ${new Date(startTime * 1000)} to ${new Date(endTime * 1000)}
+     _________________________________`
   );
   console.log(events);
 }
 
-// Run the function
+// Run the script
 getEventsByIndexedAddress(
   CONTRACT_ADDRESS,
   EVENT_NAME,
