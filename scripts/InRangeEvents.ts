b767277e6b1d5a15fbc5722071f05b69739d4f62
Samuil Borisov <samuil.bo5@gmail.com>
2024-04-24 15:49:08 +0300

    fix script for events and remove transaction script

--- scripts/InRangeEvents.ts
+++ scripts/InRangeEvents.ts
@@ -2,43 +2,49 @@
 import { ethers } from "hardhat";
 
 // eslint-disable-next-line no-unused-vars
-const TIMESTAMP = 1713537000; // 17:30 19.04.2024
+const EXAMPLE_BLOK = 1300500; // 17:23 19.04.2024 1300500 block
 
 // Input parameters for the function:
 const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000105";
-const EVENT_NAME = "ValidatorRewardDistributed";
-const VALIDATOR_ADDRESS = "0x";
-const YOUR_START_TIME_IN_UNIX_TIMESTAMP = TIMESTAMP - 1000;
-const YOUR_END_TIME_IN_UNIX_TIMESTAMP = TIMESTAMP;
+const CONTRACT_NAME = "RewardPool";
+const EVENT = "ValidatorRewardDistributed(address indexed validator, uint256 amount)";
+const VALIDATOR_ADDRESS = "PUT ADDRESS HERE";
+const YOUR_START_BLOCK = EXAMPLE_BLOK - 1000;
+const YOUR_END_BLOCK = EXAMPLE_BLOK;
 
 async function getEventsByIndexedAddress(
   contractAddress: string,
-  eventName: string,
+  contractName: string,
+  event: string,
   indexedAddress: string,
-  startTime: number,
-  endTime: number
+  startBlock: number,
+  endBlock: number
 ) {
   const provider = ethers.provider;
+  const ContractFactory = await ethers.getContractFactory(contractName);
+  const contractInstance = ContractFactory.attach(contractAddress);
+
+  // Use the contract's interface to encode the event signature
+  const encodedEvent = contractInstance.interface.getEventTopic(event);
+  // eslint-disable-next-line no-unused-vars
   const encodedAddress = ethers.utils.defaultAbiCoder.encode(["address"], [indexedAddress]);
 
   const filter = {
     address: contractAddress,
-    topics: [ethers.utils.id(eventName), encodedAddress],
+    topics: [encodedEvent, encodedAddress], // if you wanna filter all, remove encodedAddress and put null
+    fromBlock: startBlock,
+    toBlock: endBlock,
   };
 
-  const events = await provider.getLogs(
-    Object.assign({}, filter, {
-      fromBlock: startTime,
-      toBlock: endTime,
-    })
-  );
+  const events = await provider.getLogs(filter);
 
   console.log(
     ` 
      _________________________________
 
-Events for ${eventName} with indexed address ${indexedAddress} 
-from ${new Date(startTime * 1000)} to ${new Date(endTime * 1000)}
+Events for ${event} 
+indexing ${indexedAddress} 
+from ${startBlock} to ${endBlock} block
      _________________________________`
   );
   console.log(events);
@@ -47,10 +53,11 @@ from ${new Date(startTime * 1000)} to ${new Date(endTime * 1000)}
 // Run the script
 getEventsByIndexedAddress(
   CONTRACT_ADDRESS,
-  EVENT_NAME,
+  CONTRACT_NAME,
+  EVENT,
   VALIDATOR_ADDRESS,
-  YOUR_START_TIME_IN_UNIX_TIMESTAMP,
-  YOUR_END_TIME_IN_UNIX_TIMESTAMP
+  YOUR_START_BLOCK,
+  YOUR_END_BLOCK
 ).catch((error) => {
   console.error(error);
   process.exitCode = 1;
