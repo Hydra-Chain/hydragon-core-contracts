13aa7ceafd2c730f980ac39cfd68bce9acf986f4
Samuil Borisov <88675952+SamBorisov@users.noreply.github.com>
2024-05-09 12:35:49 +0300

    Feat/script (#13)
    * scripts update
    
    * signed blocks for user from block a to b
    
    * Finish scripts
    
    * fix script for events and remove transaction script
    
    * clean up
    
    * easier interaction
    
    * add helper to reuse functions
    
    * fix comments
    
    * Apply PR requested Changes
    
    * fix requested changes

---
+++ scripts/SumBlocksForOne.ts
@@ -0,0 +1,67 @@
+// Run: npx hardhat run scripts/SumBlocksForOne.ts --network childTest
+import { getTransactionsByBlock, decodeTransaction } from "./_helper";
+
+// Input parameters for the script:
+const BLOCK_NUMBER = process.env.SUM_BLOCKS_FOR_ONE_BLOCK_NUMBER;
+const MAX_BLOCK_NUMBER = process.env.SUM_BLOCKS_FOR_ONE_MAX_BLOCK_NUMBER;
+const VALIDATOR_ADDRESS = process.env.SUM_BLOCKS_FOR_ONE_VALIDATOR_ADDRESS;
+const CONTRACT_ADDRESS = process.env.SUM_BLOCKS_FOR_ONE_CONTRACT_ADDRESS;
+const FUNCTION_NAME = process.env.SUM_BLOCKS_FOR_ONE_FUNCTION_NAME;
+
+// Get the signed blocks for validator from each the block
+async function getDataFromBlock(_blockNumber: number) {
+  const block = await getTransactionsByBlock(_blockNumber);
+  if (!CONTRACT_ADDRESS || !FUNCTION_NAME || !VALIDATOR_ADDRESS) {
+    console.error("Environment variables are not set.");
+    process.exitCode = 1;
+    return;
+  }
+  const decodedData = await decodeTransaction(CONTRACT_ADDRESS, block.transactions[1].hash, FUNCTION_NAME);
+  if (decodedData === undefined || decodedData.uptime === undefined) {
+    return 0;
+  }
+  const signedBlocks = getAddressValue(decodedData.uptime, VALIDATOR_ADDRESS);
+  return signedBlocks;
+}
+
+// Helper function to get the number of signed blocks for a given address
+function getAddressValue(data: any, address: any) {
+  for (const element of data) {
+    if (element[0] === address) {
+      return element[1];
+    }
+  }
+  return 0; // Add 0 if address didn't sign any blocks
+}
+
+// Get the sum of signed blocks for the validator
+async function getSumBlocks() {
+  console.log(
+    ` 
+_________________________________
+
+Summing the signed blocks 
+for ${VALIDATOR_ADDRESS} 
+from ${BLOCK_NUMBER} to ${MAX_BLOCK_NUMBER} block
+_________________________________`
+  );
+  let sum: number = 0;
+  if (!BLOCK_NUMBER || !MAX_BLOCK_NUMBER) {
+    console.error("Environment variables are not set.");
+    process.exitCode = 1;
+    return;
+  }
+  for (let i = Number(BLOCK_NUMBER); i < Number(MAX_BLOCK_NUMBER); i += 500) {
+    const blocks = await getDataFromBlock(i);
+    const blockToNum = parseInt(blocks);
+    sum += blockToNum;
+    console.log(`Signed blocks: ${blocks}`);
+  }
+  console.log(`Sum of signed blocks: ${sum}`);
+}
+
+// Run the script
+getSumBlocks().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
