7add9d60716746efda6bd8867b5cc25e048c7c7e
Samuil Borisov <samuil.bo5@gmail.com>
2024-06-24 17:08:47 +0300

    add extra script for func selector

---
+++ scripts/FunctionSign.ts
@@ -0,0 +1,18 @@
+// Run: npx hardhat run scripts/FunctionSign.ts --network childTest
+import { ethers } from "hardhat";
+
+async function main() {
+  const functionName = "undelegate";
+  const parameterTypes = ["address", "uint256"];
+  const functionSignature = `${functionName}(${parameterTypes.join(",")})`;
+
+  const functionSelector = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(functionSignature)).substring(0, 10);
+
+  console.log(`Function Signature: ${functionSignature}`);
+  console.log(`Function Selector: ${functionSelector}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
