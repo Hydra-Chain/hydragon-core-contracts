2c26a95a9fbb0a5996b2e5a3d457e72f0077190a
Samuil Borisov <samuil.bo5@gmail.com>
2024-09-12 17:12:51 +0300

    optimize function

--- contracts/PriceOracle/libs/SortedPriceList.sol
+++ contracts/PriceOracle/libs/SortedPriceList.sol
@@ -22,10 +22,11 @@ library SortedPriceList {
 
         // Create a new node
         Node memory newNode = Node(price, address(0));
+        address current = self.head;
 
         // If the new price is smaller or equal to the head, insert at the head (or if the list is empty)
-        if (self.nodes[self.head].price >= price || self.head == address(0)) {
-            newNode.next = self.head;
+        if (self.nodes[current].price >= price || current == address(0)) {
+            newNode.next = current;
             self.head = validator;
             self.nodes[validator] = newNode;
             self.size++;
@@ -33,7 +34,6 @@ library SortedPriceList {
         }
 
         // Find the correct spot to insert the new node
-        address current = self.head;
         while (self.nodes[current].next != address(0) && price > self.nodes[self.nodes[current].next].price) {
             current = self.nodes[current].next;
         }
